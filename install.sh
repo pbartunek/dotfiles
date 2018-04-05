916ea0503ef710d360759cc85292f714e66dc13a
Pawel <pawel.bartunek@gmail.com>
2018-04-05 11:12:35 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+files=$(find . -maxdepth 1 -type f -not -name '*.sh' -printf "%f\n")
+echo "Installing dotfiles:" $files
+for f in $files; do
+  rm "${HOME}/.${f}" 2> /dev/null
+  ln -s "${PWD}/$f" "${HOME}/.${f}"
+done
+
+
+echo "Installing i3 config"
+rm ~/.config/i3/config 2> /dev/null
+
+if [ ! -d ${HOME}/.config/i3 ]; then
+  mkdir -p ${HOME}/.config/i3
+fi
+
+ln -s ${PWD}/i3/config ${HOME}/.config/i3/config
--- zshrc
+++ zshrc
@@ -36,4 +36,4 @@ zstyle ':completion:*' verbose true
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
 
-source /home/pawel/.docker-functions
+source ${HOME}/.docker-functions
