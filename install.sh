2d6e1fb40ab80ccb4155b0e09d46f60c993e3853
Pawel <pawel.bartunek@gmail.com>
2018-05-30 09:22:07 +0200

    added rofi config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=$(find . -maxdepth 1 -type f -not -name '*.sh' -exec basename {} \;)
+files=$(find . -maxdepth 1 -type f -not \( -iname '*.sh' -o -iname '*.md' \) -exec basename {} \;)
 echo "Installing dotfiles:" $files
 for f in $files; do
   rm "${HOME}/.${f}" 2> /dev/null
@@ -9,14 +9,15 @@ done
 
 
 if [ "$(uname -s)" == "Linux" ]; then
-  echo "Installing i3 config"
-  rm ~/.config/i3/config 2> /dev/null
 
-  if [ ! -d ${HOME}/.config/i3 ]; then
-    mkdir -p ${HOME}/.config/i3
-  fi
-
-  ln -s ${PWD}/i3/config ${HOME}/.config/i3/config
+  for c in i3 rofi; do
+    echo "Installing ${c} config"
+    rm ~/.config/${c}/config 2> /dev/null
+    if [ ! -d ${HOME}/.config/${c} ]; then
+      mkdir -p ${HOME}/.config/${c}
+    fi
+    ln -s ${PWD}/${c}/config ${HOME}/.config/${c}/config
+  done
 
   # get seccomp profile for containers running chrome/chromium 
   if [ ! -f ${HOME}/containers/chrome/chrome.json ]; then
