2e8b0af1d75a2e177f7e5cc9879c0333c32aa99b
Pawel <pawel.bartunek@gmail.com>
2018-05-30 12:01:37 +0200

    gitignore file

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-files=$(find . -maxdepth 1 -type f -not \( -iname '*.sh' -o -iname '*.md' \) -exec basename {} \;)
-echo "Installing dotfiles:" $files
+echo "Installing dotfiles:"
+files=$(find . -maxdepth 1 -type f -not \( -iname '*.sh' -o -iname '*.md' -o -iname '\.*' \) -exec basename {} \;)
 for f in $files; do
+  echo -n "${f} "
   rm "${HOME}/.${f}" 2> /dev/null
   ln -s "${PWD}/$f" "${HOME}/.${f}"
 done
 
-
 if [ "$(uname -s)" == "Linux" ]; then
-
+  echo -e "\nInstalling: "
+  # install i3 and rofi configs
   for c in i3 rofi; do
-    echo "Installing ${c} config"
+    echo -n "${c} "
     rm ~/.config/${c}/config 2> /dev/null
     if [ ! -d ${HOME}/.config/${c} ]; then
       mkdir -p ${HOME}/.config/${c}
     fi
     ln -s ${PWD}/${c}/config ${HOME}/.config/${c}/config
   done
+  echo
 
   # get seccomp profile for containers running chrome/chromium 
   if [ ! -f ${HOME}/containers/chrome/chrome.json ]; then
@@ -25,3 +27,6 @@ if [ "$(uname -s)" == "Linux" ]; then
     wget https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/docker/seccomp/chrome.json -O ${HOME}/containers/chrome/chrome.json
   fi
 fi
+
+# configure global gitignore file
+git config --global core.excludesfile ~/.gitignore
